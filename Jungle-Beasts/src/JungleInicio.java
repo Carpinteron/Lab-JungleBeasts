5f4bd46cc4fac897595b2cfb769aa4aca42c32ca
Ari <isabellajuliao@uninorte.edu.co>
2023-11-05 20:41:42 -0500

    sombreado check

--- Jungle-Beasts/src/JungleInicio.java
+++ Jungle-Beasts/src/JungleInicio.java
@@ -1,10 +1,17 @@
 
 import java.awt.Color;
+import javax.swing.ImageIcon;
 
 
 public class JungleInicio extends javax.swing.JFrame {
 
-
+ClassLoader CL = getClass().getClassLoader();
+    ImageIcon j = new ImageIcon(CL.getResource("IMAGENES/j.png"));
+    ImageIcon j1 = new ImageIcon(CL.getResource("IMAGENES/j1.png"));
+    ImageIcon cj = new ImageIcon(CL.getResource("IMAGENES/cj.png"));
+    ImageIcon cj1 = new ImageIcon(CL.getResource("IMAGENES/cj1.png"));
+    ImageIcon cr = new ImageIcon(CL.getResource("IMAGENES/cr.png"));
+    ImageIcon cr1 = new ImageIcon(CL.getResource("IMAGENES/cr1.png"));
     public JungleInicio() {
         initComponents();
         setBackground(new Color(0, 0, 0, 0));
@@ -19,7 +26,9 @@ public class JungleInicio extends javax.swing.JFrame {
         panelRound1 = new custom.PanelRound();
         panelRound2 = new custom.PanelRound();
         BtnEXIT = new javax.swing.JButton();
+        cred = new javax.swing.JButton();
         inicio = new javax.swing.JButton();
+        info = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -28,6 +37,7 @@ public class JungleInicio extends javax.swing.JFrame {
         panelRound1.setPreferredSize(new java.awt.Dimension(1180, 664));
 
         panelRound2.setBackground(new java.awt.Color(29, 91, 49));
+        panelRound2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         BtnEXIT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/icons8-cerca-35.png"))); // NOI18N
         BtnEXIT.setBorder(null);
@@ -39,45 +49,67 @@ public class JungleInicio extends javax.swing.JFrame {
                 BtnEXITActionPerformed(evt);
             }
         });
+        panelRound2.add(BtnEXIT, new org.netbeans.lib.awtextra.AbsoluteConstraints(1132, 6, 44, -1));
 
+        cred.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMAGENES/cr.png"))); // NOI18N
+        cred.setBorderPainted(false);
+        cred.setContentAreaFilled(false);
+        cred.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        cred.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                credMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                credMouseExited(evt);
+            }
+        });
+        cred.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                credActionPerformed(evt);
+            }
+        });
+        panelRound2.add(cred, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 550, 210, 70));
+
+        inicio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMAGENES/j.png"))); // NOI18N
         inicio.setBorderPainted(false);
         inicio.setContentAreaFilled(false);
         inicio.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        inicio.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                inicioMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                inicioMouseExited(evt);
+            }
+        });
         inicio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 inicioActionPerformed(evt);
             }
         });
+        panelRound2.add(inicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 390, 210, 70));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMAGENES/inicio.png"))); // NOI18N
+        info.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMAGENES/cj.png"))); // NOI18N
+        info.setBorderPainted(false);
+        info.setContentAreaFilled(false);
+        info.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        info.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                infoMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                infoMouseExited(evt);
+            }
+        });
+        info.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                infoActionPerformed(evt);
+            }
+        });
+        panelRound2.add(info, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 470, 210, 70));
 
-        javax.swing.GroupLayout panelRound2Layout = new javax.swing.GroupLayout(panelRound2);
-        panelRound2.setLayout(panelRound2Layout);
-        panelRound2Layout.setHorizontalGroup(
-            panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound2Layout.createSequentialGroup()
-                .addGroup(panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(1132, 1132, 1132)
-                        .addComponent(BtnEXIT, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(70, 70, 70)
-                        .addComponent(inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jLabel1))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        panelRound2Layout.setVerticalGroup(
-            panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound2Layout.createSequentialGroup()
-                .addGap(6, 6, 6)
-                .addComponent(BtnEXIT)
-                .addGap(6, 6, 6)
-                .addGroup(panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(343, 343, 343)
-                        .addComponent(inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 617, javax.swing.GroupLayout.PREFERRED_SIZE)))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMAGENES/inicio2.png"))); // NOI18N
+        panelRound2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 47, -1, 617));
 
         javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
         panelRound1.setLayout(panelRound1Layout);
@@ -112,11 +144,43 @@ public class JungleInicio extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_BtnEXITActionPerformed
 
+    private void credActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_credActionPerformed
+       
+    }//GEN-LAST:event_credActionPerformed
+
+    private void credMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_credMouseEntered
+       cred.setIcon(cr1);
+    }//GEN-LAST:event_credMouseEntered
+
+    private void credMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_credMouseExited
+        cred.setIcon(cr);
+    }//GEN-LAST:event_credMouseExited
+
+    private void inicioMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_inicioMouseEntered
+        inicio.setIcon(j1);
+    }//GEN-LAST:event_inicioMouseEntered
+
+    private void inicioMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_inicioMouseExited
+        inicio.setIcon(j);
+    }//GEN-LAST:event_inicioMouseExited
+
     private void inicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inicioActionPerformed
-        Avatars A = new Avatars();
+         Avatars A = new Avatars();
         A.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_inicioActionPerformed
+
+    private void infoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_infoMouseEntered
+        info.setIcon(cj1);
+    }//GEN-LAST:event_infoMouseEntered
+
+    private void infoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_infoMouseExited
+        info.setIcon(cj);
+    }//GEN-LAST:event_infoMouseExited
+
+    private void infoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_infoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_infoActionPerformed
 
   
     public static void main(String args[]) {
@@ -153,6 +217,8 @@ public class JungleInicio extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnEXIT;
+    private javax.swing.JButton cred;
+    private javax.swing.JButton info;
     private javax.swing.JButton inicio;
     private javax.swing.JLabel jLabel1;
     private custom.PanelRound panelRound1;
