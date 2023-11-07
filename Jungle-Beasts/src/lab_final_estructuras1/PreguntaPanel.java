da8a31d55c3859730a6e44dc6acdf89542edd0c5
Canela <carpinteron@uninorte.edu.co>
2023-11-07 18:57:30 -0500

    panel de las preguntas

--- Jungle-Beasts/src/lab_final_estructuras1/PreguntaPanel.java
+++ Jungle-Beasts/src/lab_final_estructuras1/PreguntaPanel.java
@@ -9,9 +9,10 @@ public class PreguntaPanel extends javax.swing.JPanel {
   
     public PreguntaPanel() {
         initComponents();
-        //this.setLocationRelativeTo(null); //Establece la ubicacion de la ventana en el centro de la pantalla.
+       //this.setLocationRelativeTo(null); //Establece la ubicacion de la ventana en el centro de la pantalla.
         setBackground(new Color(0, 0, 0, 0)); //Le establece el color del fondo de la ventana a transparente.
-      //  BARRA.setBackground(new Color(0, 0, 0, 0));
+       // BARRA.setBackground(new Color(0, 0, 0, 0)); 
+       
     }
 
     @SuppressWarnings("unchecked")
@@ -24,7 +25,6 @@ public class PreguntaPanel extends javax.swing.JPanel {
         BTNa = new javax.swing.JButton();
         BTNb = new javax.swing.JButton();
         LabelIMG = new javax.swing.JLabel();
-        BARRA = new javax.swing.JPanel();
         BtnEXIT = new javax.swing.JButton();
 
         setLayout(null);
@@ -60,19 +60,6 @@ public class PreguntaPanel extends javax.swing.JPanel {
         add(LabelIMG);
         LabelIMG.setBounds(710, 60, 440, 270);
 
-        BARRA.setBackground(new java.awt.Color(255, 255, 255));
-        BARRA.setForeground(new java.awt.Color(255, 255, 255));
-        BARRA.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseDragged(java.awt.event.MouseEvent evt) {
-                BARRAMouseDragged(evt);
-            }
-        });
-        BARRA.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                BARRAMousePressed(evt);
-            }
-        });
-
         BtnEXIT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/icons8-cerca-35.png"))); // NOI18N
         BtnEXIT.setBorder(null);
         BtnEXIT.setBorderPainted(false);
@@ -83,26 +70,8 @@ public class PreguntaPanel extends javax.swing.JPanel {
                 BtnEXITActionPerformed(evt);
             }
         });
-
-        javax.swing.GroupLayout BARRALayout = new javax.swing.GroupLayout(BARRA);
-        BARRA.setLayout(BARRALayout);
-        BARRALayout.setHorizontalGroup(
-            BARRALayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BARRALayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(BtnEXIT, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-        BARRALayout.setVerticalGroup(
-            BARRALayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BARRALayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(BtnEXIT)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        add(BARRA);
-        BARRA.setBounds(0, 0, 1180, 47);
+        add(BtnEXIT);
+        BtnEXIT.setBounds(1140, 10, 35, 35);
     }// </editor-fold>//GEN-END:initComponents
 
     private void BTNcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNcActionPerformed
@@ -113,20 +82,8 @@ public class PreguntaPanel extends javax.swing.JPanel {
         System.exit(0);
     }//GEN-LAST:event_BtnEXITActionPerformed
 
-    private void BARRAMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BARRAMouseDragged
-        int x = evt.getXOnScreen(); //Obttiene la posicion horizontal actual en pantalla.
-        int y = evt.getYOnScreen(); //Obtiene la posicion vertical actual en pantalla.
-        this.setLocation(x - xmouse, y - ymouse); //Establece la poscion de la ventana en funcion de la posicion original del mouse.
-    }//GEN-LAST:event_BARRAMouseDragged
-
-    private void BARRAMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BARRAMousePressed
-        xmouse = evt.getX(); // Obtiene la posicion horizontal del punto donde se hizo clic.
-        ymouse = evt.getY(); // Obtiene la posicion vertical del punto donde se hizo clic.
-    }//GEN-LAST:event_BARRAMousePressed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel BARRA;
     private javax.swing.JButton BTNa;
     private javax.swing.JButton BTNb;
     private javax.swing.JButton BTNc;
